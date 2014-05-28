1bd9c8eaa2a7364f91f6e23bd4630323e070dde5
Adam Paterson <hello@adampaterson.co.uk>
2014-05-28 12:18:22 +0100

    Added model sniff

---
+++ Magento/Sniffs/Performance/ModelLoadSniff.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Check for model instantiation in template files.
+ * Class Magento_Sniffs_Performance_ModelLoadSniff
+ * @author Adam Paterson <adam@wearejh.com>
+ */
+class Magento_Sniffs_Performance_ModelLoadSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * Methods to check against.
+     * @var array
+     */
+    protected $methods = array(
+        'getModel'
+    );
+
+    /**
+     * Returns the token types that this sniff is interested in.
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_DOUBLE_COLON);
+    }
+
+    /**
+     * @param PHP_CodeSniffer_File $phpcsFile
+     * @param int $stackPtr
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        if (!in_array($tokens[$stackPtr + 1]['content'], $this->methods)) {
+            return;
+        }
+
+        $error = 'A model should not be loaded in a template. Found: %s';
+
+        /** @var $model find the model class for reporting */
+        $model = $phpcsFile->findNext(T_CONSTANT_ENCAPSED_STRING, $stackPtr);
+        $data = array(trim($tokens[$model]['content']));
+        $phpcsFile->addError($error, $stackPtr, 'Found', $data);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Magento_CodeSniffer
 ===================
+
+[![Build Status](https://travis-ci.org/adam-paterson/magento_codesniffer.png?branch=master)(http://travis-ci.org/adam-paterson/magento_codesniffer)
